fd668e6a7fb8d38d95dea2e6b524a11f70f16d7d
Robin A. P <me@mempler.de>
2021-05-04 11:19:35 +0000

    Flip UVs

--- game/src/main.cc
+++ game/src/main.cc
@@ -18,17 +18,16 @@ protected:
         Box *box = GetEngine()->GetGUI()->Add<Box>("gui://random_box");
         box->m_v2Position = { 100.0f, 100.0f };
         box->m_v4Color = { 1.0f, 0.0f, 0.0f, 1.0f };
-        box->m_v2Size = { 100.f, 100.f };
+        box->m_v2Size = { 500.f, 500.f };
         box->m_eOrigin = Origin::Center;
 
         box->CalculateTransformation();
 
         //box->RotateTo(360)->Easing(EasingType::circularInOut)->Within(1000)->Repeat()->Easing(EasingType::backInOut)->Repeat();
-        box->ScaleTo({ 1.5, 1.5 })->Easing(EasingType::Linear)->Within(1000)->Repeat()->Repeat();
+        //box->ScaleTo({ 1.5, 1.5 })->Easing(EasingType::Linear)->Within(1000)->Repeat()->Repeat();
 
-        /*
         box->FadeTo({ 0.5, 0.5, 1.0, 1.0 })->Easing(EasingType::elasticOut)->Within(1000);
-        box->SetImage("file://yes.png");*/
+        box->SetImage("file://yes.png");
     }
 
     void Tick(float fDelta) override {
